9b775e131dbce200b20188f5c3c871eb6e0d19f3
razvansdira <sdira_razvan@yahoo.com>
2018-06-26 11:11:15 +0300

    New company register final

--- src/test/java/de/gourmondo/pages/HmcPages.java
+++ src/test/java/de/gourmondo/pages/HmcPages.java
@@ -2,22 +2,19 @@ package de.gourmondo.pages;
 
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.List;
 
 
 public class HmcPages extends PageObject {
-
-    WebDriver driver;
 
     @FindBy(css = "#Tree\\2f GenericExplorerMenuTreeNode\\5b user\\5d _label")
     private WebElementFacade userTab;
@@ -40,6 +37,9 @@ public class HmcPages extends PageObject {
     @FindBy(xpath = "//*[@id=\"outerTD\"]/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/div[1]/table/tbody/tr[1]/td/table/tbody/tr/td[2]/table/tbody/tr/td[1]")
     private WebElementFacade saveButton;
 
+    @FindBy(css = "[id=\"Content/OrganizerItemChip$2[organizer.editor.delete.title]_label\"]")
+    private WebElementFacade deleteButton;
+
 
     public void clickOnUserTab() {userTab.click();}
 
@@ -51,9 +51,7 @@ public class HmcPages extends PageObject {
 
     public void clickOnResult(){result.click();}
 
-    public void clickOnOptions(){
-        options.click();
-    }
+    public void clickOnOptions(){options.click();}
 
     public void clickOnApprovedStatus(String status){
 
@@ -69,4 +67,17 @@ public class HmcPages extends PageObject {
     }
 
     public void saveOptions(){saveButton.click();}
+
+    public void deleteUser(){deleteButton.click();}
+
+    public void confirmDelete(){
+        try {
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            robot.delay(200);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
 }
